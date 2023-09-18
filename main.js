ad00ec0faf88d4fbdf422ff3f3e68996fe146801
G-MSTELC <121738818+G-MSTELC@users.noreply.github.com>
2023-09-18 03:34:42 -0400

    troisieme commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+console.log("Bonjour, ceci est un exemple de code JavaScript.");
+let nombre1 = 5;
+let nombre2 = 10;
+let somme = nombre1 + nombre2;
+let multip = nombre1 *nombre2;
+console.log("La somme de", nombre1, "et", nombre2, "est égale à", somme);
+console.log(multip,"est le produit des deux nombres");
