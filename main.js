ea6ef9770b8e34253ded0b5d686d56b34b598780
G-MSTELC <121738818+G-MSTELC@users.noreply.github.com>
2023-09-18 03:59:09 -0400

    dernier changement

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ let nombre1 = 5;
 let nombre2 = 10;
 let somme = nombre1 + nombre2;
 let multip = nombre1 *nombre2;
+let soustraction =nombre1-nombre2;
 console.log("La somme de", nombre1, "et", nombre2, "est égale à", somme);
 console.log(multip,"est le produit des deux nombres");
